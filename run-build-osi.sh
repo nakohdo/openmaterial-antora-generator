51337b5a4c428aa8a161c0c5778350706934c704
Frank Ralf <frank.ralf@parson-europe.com>
2024-08-28 15:55:46 +0200

    Update configuration.

--- run-build-osi.sh
+++ run-build-osi.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+# Template: https://code.asam.net/common/templates/antora-base-repo-example/-/blob/standard_generator/run-build.sh
+# Modified based on OSI version: https://github.com/OpenSimulationInterface/osi-antora-generator/blob/main/run-build.sh
 # This script is used tackle a some specifics of the GitHub action used with a docker container. 
 # It copies the cached node-modules from the container to the runner workspace and then triggers the build using npm
 
